034e3a7e24bd19613b5d652a48855532e25a8799
valerieoniku <valerieabby12@gmail.com>
2026-04-10 19:50:01 -0400

    Added update method for ball movement and wall bouncing

--- Lab 4/Lab 4 part 3/main.js
+++ Lab 4/Lab 4 part 3/main.js
@@ -29,3 +29,23 @@ class Ball {
     ctx.fill();
   }
 }
+update() {
+  if (this.x + this.size >= width) {
+    this.velX = -this.velX;
+  }
+
+  if (this.x - this.size <= 0) {
+    this.velX = -this.velX;
+  }
+
+  if (this.y + this.size >= height) {
+    this.velY = -this.velY;
+  }
+
+  if (this.y - this.size <= 0) {
+    this.velY = -this.velY;
+  }
+
+  this.x += this.velX;
+  this.y += this.velY;
+}
