534f7a6bba0d4e605c00f3507bbf85b201f8f9bc
valerieoniku <valerieabby12@gmail.com>
2026-04-10 19:46:54 -0400

    Initial commit - added bouncing balls starter files

---
+++ Lab 4/Lab 4 part 3/main.js
@@ -0,0 +1,14 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
+
+// helper functions
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomRGB() {
+  return `rgb(${random(0,255)} ${random(0,255)} ${random(0,255)})`;
+}
