ee95a53e453c08b658e1cf563302d9643ae1f3c1
valerieoniku <valerieabby12@gmail.com>
2026-04-10 16:18:38 -0400

    Added arrays and random story function

---
+++ Lab 4/Lab 4 part 1/main.js
@@ -0,0 +1,71 @@
+// Complete variable definitions and random functions
+
+const customName = document.getElementById("custom-name");
+const generateBtn = document.querySelector(".generate");
+const story = document.querySelector(".story");
+
+function randomValueFromArray(array) {
+  const random = Math.floor(Math.random() * array.length);
+  return array[random];
+}
+
+// Raw text strings
+
+const characters = [
+  "Willy the Goblin",
+  "Big Daddy",
+  "Father Christmas"
+];
+
+const places = [
+  "the soup kitchen",
+  "Disneyland",
+  "the White House"
+];
+
+const events = [
+  "spontaneously combusted",
+  "melted into a puddle on the sidewalk",
+  "turned into a slug and slithered away"
+];
+
+// Return random story function
+
+function returnRandomStoryString() {
+
+  const randomCharacter = randomValueFromArray(characters);
+  const randomPlace = randomValueFromArray(places);
+  const randomEvent = randomValueFromArray(events);
+
+  const storyText = `It was 94 Fahrenheit outside, so ${randomCharacter} went for a walk. When they got to ${randomPlace}, they stared in horror for a few moments, then ${randomEvent}. Bob saw the whole thing, but was not surprised — ${randomCharacter} weighs 300 pounds, and it was a hot day.`;
+
+  return storyText;
+}
+
+// Event listener
+
+generateBtn.addEventListener("click", generateStory);
+
+function generateStory() {
+
+  let newStory = returnRandomStoryString();
+
+  // Custom name
+  if (customName.value !== "") {
+    const name = customName.value;
+    newStory = newStory.replace("Bob", name);
+  }
+
+  // UK conversion
+  if (document.getElementById("uk").checked) {
+
+    const weight = Math.round(300 / 14) + " stone";
+    const temperature = Math.round((94 - 32) * 5 / 9) + " Celsius";
+
+    newStory = newStory.replace("300 pounds", weight);
+    newStory = newStory.replace("94 Fahrenheit", temperature);
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = "visible";
+}
